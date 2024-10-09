ede82d4a1aea39c1173e331f4dbe7f8edd0e0328
Yogina Khandelwal <yogina.khandelwal@gmail.com>
2024-10-08 20:11:33 -0400

    added stylling changes

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import Category from '../../components/Category/Category'
-import './Menu.scss'
+import React from "react";
+import { useEffect, useState } from "react";
+import Category from "../../components/Category/Category";
+import "./Menu.scss";
 
 export default function Menu() {
-    const [categories, setCategories] = useState([])
-    const displayFood = async () => {
-      const res = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-      const data = await res.json()
-      setCategories(data.categories)
-    }
-    useEffect(() => {
-      displayFood()
-    }, [])
+  const [categories, setCategories] = useState([]);
+  const displayFood = async () => {
+    const res = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/categories.php"
+    );
+    const data = await res.json();
+    setCategories(data.categories);
+  };
+  useEffect(() => {
+    displayFood();
+  }, []);
 
   return (
-    <section id='menu'>
-       
-    <div id='categoryContainer' className='category container'>    
-    {
-      categories.map((category)=>{
-        return <Category key={category.idCategory} category={category}/>
-      })
-    }
-  </div>
-</section>
-  )
+    <section id="menu">
+      <div id="categoryContainer" className="category container">
+        {categories.map((category) => {
+          return <Category key={category.idCategory} category={category} />;
+        })}
+      </div>
+    </section>
+  );
 }
